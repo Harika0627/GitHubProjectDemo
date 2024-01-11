455e7089c0cf6d02c904f9521ef49861af57da60
Harika D <harika1.d@gmail.com>
2024-01-11 16:30:51 +0530

    First Commit with few files modified

---
+++ src/packagesJava/FirstClassDemo.java
@@ -0,0 +1,23 @@
+package packagesJava;
+
+public class FirstClassDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Java");
+//        System.out.println("Welcome Again");
+        System.out.println("100");
+        System.out.println(100);
+        System.out.println("My Fav Num is" + (100+200));
+        
+    /*    I am trying to learn the comments
+        and some
+        javaconcepts
+        
+        */
+        
+        System.out.println("Iam done commenting");
+        
+	}
+
+}
